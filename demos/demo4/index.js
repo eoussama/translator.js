ad790bdf965e53b53f056e4895612e6ad3bd5e4b
EOussama <oussama_gtacj@live.fr>
2020-01-04 16:39:03 +0100

    Added more implementation in demo4

--- demos/demo4/index.js
+++ demos/demo4/index.js
@@ -1,17 +1,67 @@
+// Importing EO TranslatorJS 
 var EOTranslator = require('./../assets/js/eo-translator.min');
 
+// Creating a dictionary
 var dict = {
   'en': {
-    'greet': 'Hello {name}!!'
+    'greet': 'Hello {name}!!',
+    'tmp': 'tmp'
   },
   'fr': {
     'greet': 'Bonjour {name}!!'
   }
 }
 
+// Creating a translator
 var translator = new EOTranslator(dict);
 
-translator.translateDOM();
-translator.translateElement();
+// Displaying the dictionary
+console.log({ dictionary: dict });
 
-console.log(translator.translate('greet', { params: { name: 'Oussama' } }));
+// Translating the greeting in English...
+var translation = translator.translate('greet', { params: { name: 'Oussama' } });
+
+// Displaying the translation
+console.log({ 'English greeting': translation });
+
+// Translating the greeting in French...
+translation = translator.translate('greet', { lang: 'fr', params: { name: 'Oussama' } });
+
+// Displaying the translation
+console.log({ 'French greeting': translation });
+
+// Removing the `tmp` translation key
+translator.remove('en', 'tmp');
+
+// Displaying the dictionary
+console.log({ dictionary: translator.dictionary });
+
+// Updating the `greet` translation key in English
+translator.add('en', 'greet', 'Hey, {name}! how are you?');
+
+// Translating the greeting to French...
+translation = translator.translate('greet', { params: { name: 'Oussama' } });
+
+// Displaying the translation
+console.log({ 'Updated English greeting': translation });
+
+// Adding a new English translation, `home`
+translator.add('en', 'home', 'My house is {width} {unit} wide.');
+
+// Translating the `home` in English...
+translation = translator.translate('home', { params: { width: 123, unit: 'meters' } });
+
+// Displaying the translation
+console.log({ 'Home': translation });
+
+// Adding a nested Arabic translation
+translator.add('ar', 'nested.translation.greet', 'مرحبا');
+
+// Translating the Arabic greeting...
+translation = translator.translate('greet', { lang: 'ar' });
+
+// Displaying the translation
+console.log({ 'Home': translation });
+
+// Displaying the dictionary
+console.log({ dictionary: translator.dictionary });
