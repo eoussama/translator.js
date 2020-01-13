1f5efac8996db5a7ca88f69bddfc43a5fab86be2
EOussama <oussama_gtacj@live.fr>
2020-01-13 20:26:36 +0100

    Added testing on basic translations

--- tests/utils/translations.js
+++ tests/utils/translations.js
@@ -1,1 +1,25 @@
-module.exports = function (EOTranslator) { };
+module.exports = function (EOTranslator) {
+  test('Basic translation.', function () {
+
+    // Arrange
+    var translator;
+    var translation = '';
+    var dict = {
+      en: {
+        greet: 'Hello!'
+      }
+    }
+
+    // Act
+    try {
+      translator = new EOTranslator(dict);
+      translation = translator.translate('greet');
+    }
+    catch (e) {
+      console.error({ error: e });
+    }
+
+    // Assert
+    expect(translation).toEqual('Hello!');
+  });
+};
