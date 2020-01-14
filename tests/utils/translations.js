1e39b2dd78be7a82208cf9f9aa888bca4f512c96
EOussama <oussama_gtacj@live.fr>
2020-01-14 20:33:24 +0100

    Added testing on nested translations

--- tests/utils/translations.js
+++ tests/utils/translations.js
@@ -22,4 +22,31 @@ module.exports = function (EOTranslator) {
     // Assert
     expect(translation).toEqual('Hello!');
   });
+
+  test('Nested translation.', function () {
+
+    // Arrange
+    var translator;
+    var translation = '';
+    var dict = {
+      en: {
+        greet: {
+          morning: 'Good morning!',
+          evening: 'Good evening!'
+        }
+      }
+    }
+
+    // Act
+    try {
+      translator = new EOTranslator(dict);
+      translation = translator.translate('greet.evening');
+    }
+    catch (e) {
+      console.error({ error: e });
+    }
+
+    // Assert
+    expect(translation).toEqual('Good evening!');
+  });
 };
